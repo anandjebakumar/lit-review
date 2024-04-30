84d29736712266f61e75e251f9acbd982985ce45
ANAND SAMUEL JEBAKUMAR <anand.samuel.jebakumar@hp.com>
2024-04-30 08:25:02 -0700

    added saved reviews into cards and homes there

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Lit review
 
 My first implementation of a web app that allows users to save reviews of journal articles.
+
+## things to do 
+
+- [] home page
+- [] make each review its own page
+- [] add email to login
+- [] add star for review
--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import time
 
 all_data = []
 
-
-def save_review():
-    print(title.value)
+def save_review(components):
+    title, authors, journal, date, abstract, rating, review = components
+    # print(title.value)
     ui.notify('saved review!')
     data = dict()
     data['title'] = title.value
@@ -21,6 +21,30 @@ def save_review():
     with open('data.yml', 'a') as outfile:
         yaml.dump(all_data, outfile)
     ui.navigate.to('/')
+
+@ui.page('/')
+def home():
+    with open('data.yml', 'r') as file:
+        data = yaml.safe_load(file)
+    ui.label('saved reviews').style('font-size:200%')
+    
+    for review in data:
+        with ui.card():
+            ui.label(review['title']).style('font-size:120%')
+            ui.label(review['authors']).style('font-size:120%')
+            ui.label(review['journal']).style('font-size:120%')
+            ui.label(review['publication_date']).style('font-size:120%')
+            ui.label(review['abstract']).style('font-size:120%')
+            ui.label(f'rating - {review["rating"]}').style('font-size:120%')
+            ui.label(review['review']).style('font-size:120%')
+            authors = review['authors']
+            journal = review['journal']
+            date = review['publication_date']
+            abstract = review['abstract']
+            rating = review['rating']
+            review = review['review']
+
+    ui.button('add review', on_click=ui.notify('clicked')).style('font-size:120%')
 
 @ui.page('/add_review')
 def add_review():
@@ -40,29 +64,12 @@ def add_review():
 
     review = ui.textarea(label='review').classes('w-full h-full large-textarea')
 
-    save = ui.button('save review', on_click=save_review)
+    components = [title, authors, journal, date, abstract, rating, review]
+    save = ui.button('save review', on_click= lambda: save_review(components))
 
     # define some custom CSS to make the textarea fill the height of the QInput component
     ui.add_head_html('<style>.large-textarea .q-field__control { height: 100%; } </style>')
 
 
-with open('data.yml', 'r') as file:
-    data = yaml.safe_load(file)
-ui.label('saved reviews').style('font-size:200%')
-ui.button('add review', on_click=ui.navigate.to('/add_review')).style('font-size:120%')
-for review in data:
-    ui.label(review['title']).style('font-size:120%')
-    ui.label(review['authors']).style('font-size:120%')
-    ui.label(review['journal']).style('font-size:120%')
-    ui.label(review['publication_date']).style('font-size:120%')
-    ui.label(review['abstract']).style('font-size:120%')
-    ui.label(f'rating - {review["rating"]}').style('font-size:120%')
-    ui.label(review['review']).style('font-size:120%')
-    authors = review['authors']
-    journal = review['journal']
-    date = review['publication_date']
-    abstract = review['abstract']
-    rating = review['rating']
-    review = review['review']
 
 ui.run()
